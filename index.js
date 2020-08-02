4df2171e2ab651747af1c53194b2690b98a73ba9
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-08-01 21:16:50 -0400

    validation added for mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const debug = require("debug")("app:startup");
 //const dbDebugger = require("debug")("app:db");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
@@ -21,8 +22,17 @@ app.use(helmet()); //to secure http request
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); // to server static content
 
-// routes setup
+// mongoose connection'
 
+mongoose
+  .connect("mongodb://localhost/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("conneted to mogodb.."))
+  .catch((err) => console.log(err.message));
+
+// routes setup
 app.use("/api/courses", courses);
 app.use("/", home);
 // config
--- routes/courses.js
+++ routes/courses.js
@@ -1,50 +1,68 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
+const mongoose = require("mongoose");
 
-const courses = [
-  { id: 1, name: "krishna" },
-  { id: 2, name: "kanth" },
-  { id: 3, name: "kondoju" },
-];
+const Course = mongoose.model(
+  "courses",
+  new mongoose.Schema({
+    name: { type: String, required: true, minlength: 5, maxlength: 50 },
+    phone: { type: Number, required: true, minlength: 9 },
+    isGold: Boolean,
+    // category: {
+    //   type: String,
+    //   enum: ["JS", "WEB", "ANGULAR"],
+    //   required: true,
+    //   trim: true,
+    // },
+    category: Array,
+  })
+);
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const courses = await Course.find().sort("name");
   res.send(courses);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validateCouse(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  const course = {
-    id: courses.length + 1,
+  let course = new Course({
     name: req.body.name,
-  };
-  courses.push(course);
+    phone: req.body.phone,
+    isGold: req.body.isGold,
+    category: req.body.category,
+  });
+  course = await course.save();
   res.send(course);
 });
 
-router.put("/:id", (req, res) => {
-  let course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("The course id not found");
+router.put("/:id", async (req, res) => {
   const { error } = validateCouse(req.body);
   if (error) {
     res.status(400).send(error.details[0].message);
     return;
   }
-  course.name = (req && req.body && req.body.name) || null;
-  res.send(course);
-});
+  const course = await Course.findByIdAndUpdate(
+    req.param.id,
+    { name: req.body.name },
+    {
+      new: true,
+    }
+  );
 
-router.delete("/:id", (req, res) => {
-  let course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) return res.status(404).send("The course id not found");
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
   res.send(course);
 });
 
-router.get("/:id", (req, res) => {
-  let course = courses.find((c) => c.id === parseInt(req.params.id));
+router.delete("/:id", async (req, res) => {
+  const course = await Course.findByIdAndRemove(req.params.id);
+  if (!course) return res.status(404).send("The course id not found");
+  res.send(course);
+});
+
+router.get("/:id", async (req, res) => {
+  const course = await Course.findById(req.params.id);
   if (!course) return res.status(404).send("The course id not found"); // 404 not found
   res.send(course);
 });
@@ -52,6 +70,9 @@ router.get("/:id", (req, res) => {
 function validateCouse(arg) {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
+    phone: Joi.number(),
+    isGold: Joi.boolean(),
+    category: Joi.array(),
   });
   return schema.validate(arg);
 }
