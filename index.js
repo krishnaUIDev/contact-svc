41cb51e89863d86ab966384922f29597e574d19a
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-08-13 18:38:42 -0400

    Added dummy host

--- index.js
+++ index.js
@@ -21,5 +21,11 @@ require("./startup/prod")(app);
 app.set("view engine", "pug");
 app.set("views", "./views"); // to set default template
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => winston.info(`Listing on port ${port}`));
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => winston.info(`Listing on port ${port}`));
+
+const server_port = process.env.YOUR_PORT || process.env.PORT || 3001;
+const server_host = process.env.YOUR_HOST || "0.0.0.0";
+app.listen(server_port, server_host, function () {
+  console.log("Listening on port %d", server_port);
+});
