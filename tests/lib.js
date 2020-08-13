3689ed3fe610868e59af56631266b46d95ecf6d6
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-08-13 13:04:05 -0400

    added

--- tests/lib.js
+++ tests/lib.js
@@ -1,5 +1,27 @@
 module.exports.absolute = function (num) {
-  if (num > 0) return num;
-  if (num < 0) return -num;
-  return 0;
+  return num >= 0 ? num : -num;
+};
+
+module.exports.getCurrenty = function () {
+  return ["USD", "AUD", "EUR"];
+};
+
+module.exports.getProduct = function (id) {
+  return { product: id, name: "krishna" };
+};
+
+module.exports.registerUser = function (userName) {
+  if (!userName) throw new Error("User is required");
+  return {
+    id: new Date().getTime(),
+    userName,
+  };
+};
+
+module.exports.fizzBuzz = function (input) {
+  if (typeof input !== "number") throw new Error("input should be number");
+  if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
+  if (input % 3 === 0) return "Fizz";
+  if (input % 5) return "Buzz";
+  return input;
 };
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,6 +1,65 @@
 const lib = require("./lib");
 
-test("Our first test", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
+describe("first", () => {
+  test("Our first test", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+  test("Our first test", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+});
+
+describe("array", () => {
+  test("Our first test", () => {
+    const result = lib.getCurrenty();
+    expect(result).toEqual(expect.arrayContaining(["USD"]));
+  });
+});
+
+describe("obj", () => {
+  it("Our first test", () => {
+    const result = lib.getProduct(1);
+    // expect(result).toEqual({ product: 1, name: "krishna" });
+    expect(result).toMatchObject({ product: 1, name: "krishna" });
+    // expect(result).toHaveProperty("product", 1);
+  });
+});
+
+describe("exception", () => {
+  it("Our first test", () => {
+    //expect(restult).toThrow();
+    // Null undefined '' 0 false --> falsy
+    const args = [null, undefined, NaN, "", 0, false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+  it("should return object", () => {
+    const result = lib.registerUser("user");
+    expect(result).toHaveProperty("userName", "user");
+    expect(result.id).toBeGreaterThan(0);
+  });
+});
+
+describe("excercise", () => {
+  it("Our first test", () => {
+    const args = [null, undefined, NaN, "", 0, false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+  it("should return fizzBuzz", () => {
+    const result = lib.fizzBuzz(15);
+    expect(result).toBe("FizzBuzz");
+  });
+  it("should return fizzBuzz", () => {
+    const result = lib.fizzBuzz(3);
+    expect(result).toBe("Fizz");
+  });
 });
